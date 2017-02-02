83e2acd9eafb0ee83b481acf8306772b2236be9c
Marcus Krause <ma.krause@reply.de>
2017-02-02 11:03:29 +0100

    [!!!][TASK] Rename lookup worker file

--- worker/LookupWorker.php
+++ worker/LookupWorker.php
@@ -9,6 +9,9 @@ require_once $vendorDir . '/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\GelfHandler;
+use Gelf\Publisher;
+use Gelf\Transport\UdpTransport;
 
 
 $logfile = __DIR__ . '/../t3census-worker-lookup.log';
@@ -17,7 +20,7 @@ $logfile = __DIR__ . '/../t3census-worker-lookup.log';
 // create a log channel
 $logger = new Logger('t3census-worker-lookup');
 $logger->pushHandler(new StreamHandler($logfile, Logger::WARNING));
-//TODO graylog
+$logger->pushHandler(new GelfHandler(new Publisher(new UdpTransport('127.0.0.1', 12201)), Logger::DEBUG));
 
 $worker = new GearmanWorker();
 $worker->addServer('127.0.0.1', 4730);
@@ -29,6 +32,7 @@ while (1) {
 		$worker->work();
 	} catch (Exception $e) {
 		fwrite(STDERR, sprintf('ERROR: Job-Worker: %s (Errno: %u)' . PHP_EOL, $e->getMessage(), $e->getCode()));
+		$logger->addError($e->getMessage(), array('errorcode' => $e->getCode()));
 		exit(1);
 	}
 
@@ -57,15 +61,19 @@ function fetchHostnames(GearmanJob $job) {
 		$results = $objLookup->setQuery('ip:' . $ip)->getResults();
 		unset($objLookup);
 	} catch (\T3census\Bing\Scraper\Exception\EmptyBodyException $e) {
-		$logger->addWarning($e->getMessage(), array('ip' => $ip));
+		$logger->addWarning($e->getMessage(), array('errorcode' => $e->getCode(), 'ip' => $ip));
 		$job->sendData(Logger::WARNING . ' ' . $e->getMessage());
 		$job->sendFail();
 		return;
 	} catch (Exception $e) {
-		$logger->addError($e->getMessage(), array('ip' => $ip));
+		$logger->addError($e->getMessage(), array('errorcode' => $e->getCode(), 'ip' => $ip));
 		$job->sendData(Logger::ERROR . ' ' . $e->getMessage());
 		$job->sendException($e->getMessage());
 		return;
+	}
+
+	if (!empty($results)) {
+		$logger->addInfo('Retrieved hostnames for IP', array('ip' => $ip, 'urls' => $results, 'count' => count($results)));
 	}
 
 	return json_encode($results);
